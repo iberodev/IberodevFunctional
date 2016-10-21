51a81ae9d6d66b9cc7ad87a38ab46f15badf2cb3
Diego Martin <diego@speedi.co.nz>
2016-10-21 17:18:16 +1300

    Added lambda example

--- src/Iberodev.Functional.Lambda/Program.cs
+++ src/Iberodev.Functional.Lambda/Program.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Iberodev.Functional.Lambda;
+using System;
 
-namespace Iberodev.Functional.Lambda
+namespace IberodevFuncionalProgramming
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            DelegateExamples example = new DelegateExamples();
+            example.RunExampleDelegate();
+        }
+    }
+
+    public class DelegateExamples
+    {
+        public void RunExampleDelegate()
+        {
+            SmartClass smartClass = new SmartClass();
+            smartClass.MethodAddress += message => Console.WriteLine($"Using lambda the result is \n{message}");
+            smartClass.Divide(6, 0);
+            Console.ReadLine();
         }
     }
 }
