01048fa8a2ca1b1034e1ef92ead6df296e09dcba
Diego Martin <diego@speedi.co.nz>
2016-10-21 17:49:21 +1300

    Replaced delegate with my Action<T>

---
+++ src/Iberodev.Functional.Actions/SmartClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Iberodev.Functional.Action
+{
+    public class SmartClass
+    {
+        public Action<string> MethodAddress { get; set; }
+        public void Divide(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                MethodAddress?.Invoke("Cannot divide by zero");
+            }
+            else
+            {
+                int result = numerator / denominator;
+                MethodAddress?.Invoke($"The result is {result}");
+            }
+        }
+    }
+}
