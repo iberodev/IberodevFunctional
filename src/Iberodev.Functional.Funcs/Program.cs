a3a7ec9636599e506bfbc1e456a4f26f52ca8cc3
Diego Martin <iberodev@gmail.com>
2016-10-23 10:26:36 +1300

    Added func example

--- src/Iberodev.Functional.Funcs/Program.cs
+++ src/Iberodev.Functional.Funcs/Program.cs
@@ -1,14 +1,38 @@
-﻿using System;
+﻿using Iberodev.Functional.Func;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Iberodev.Functional.Funcs
+namespace IberodevFuncionalProgramming
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            DelegateExamples example = new DelegateExamples();
+            example.RunExampleDelegate();
+        }
+    }
+
+    public class DelegateExamples
+    {
+        public void RunExampleDelegate()
+        {
+            SmartClass smartClass = new SmartClass();
+            PrintWordsOnConsole("I like learning Func with Iberodev", smartClass.ConvertToListOfUpperCaseWords);
+            PrintWordsOnConsole("Hey! and now I understand delegates!", (sentence) =>
+            {
+                return sentence.ToLower().Split('!');
+            });
+            Console.ReadLine();
+        }
+
+        private void PrintWordsOnConsole(string sentence, Func<string, IEnumerable<string>> functionThatReturnsWords)
+        {
+            var words = functionThatReturnsWords(sentence);
+            foreach(var word in words)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
---
+++ src/Iberodev.Functional.Funcs/SmartClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iberodev.Functional.Func
+{
+    public class SmartClass
+    {
+        public IEnumerable<string> ConvertToListOfUpperCaseWords(string sentence)
+        {
+            return sentence.ToUpper().Split(' ');
+        }
+    }
+}
